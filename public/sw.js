1b50d44d1c0f0f2b18d4d6f719fdb13a56cdc6f5
AntonTimichev <enfild2008@yandex.ru>
2019-04-15 20:54:28 +0300

    убрал fetch servicWorker

--- public/sw.js
+++ public/sw.js
@@ -2,12 +2,9 @@ self.addEventListener(`install`, (evt) => {
   const openCache = caches.open(`STATIC_V1.0`)
     .then((cache) => {
       return cache.addAll([
-
         `/css`,
         `/images`,
         `/index.html`,
-        `/film-details.html`,
-        `statistic.html`,
       ]);
   });
   evt.waitUntil(openCache);
@@ -15,12 +12,4 @@ self.addEventListener(`install`, (evt) => {
 
 self.addEventListener(`activate`, () => {
   console.log(`sw activated`);
-});
-
-self.addEventListener(`fetch`, (evt) => {
-  evt.respondWith(
-    caches.match(evt.request)
-      .then((response) => response ? response : fetch(evt.request))
-      .catch((err) => console.error({err}))
-  );
 });
